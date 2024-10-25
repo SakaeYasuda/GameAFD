cf8d8a043e0b4bcced0ec49f3d36049c7cffe2e3
Juan <dev.juansouza@gmail.com>
2024-10-25 14:08:23 -0300

    Beta 1.3

--- color_rect.gd
+++
@@ -1,7 +0,0 @@
-extends ColorRect
-
-var threshold = 0.0
-
-# Called every frame. '_delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	material.set("shader_parameter/threshold", threshold)
